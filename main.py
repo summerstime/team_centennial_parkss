a8f151098015a46acf2223f2e1a977a11323b651
Shana Summers <78990482+summerstime@users.noreply.github.com>
2021-08-19 19:55:27 -0500

    Update main.py

--- main.py
+++ main.py
@@ -10,9 +10,9 @@ from sklearn.model_selection import train_test_split
 
 app = Flask(__name__)
 
-#db = sqla.create_engine('sqlite:///zillow.db')
-#df = pd.read_sql('SELECT * FROM zillow', db, parse_dates=['Date'])
-df = pd.read_csv('combined_zillow.csv')
+db = sqla.create_engine('sqlite:///zillow.db')
+df = pd.read_sql('SELECT * FROM zillow', db, parse_dates=['Date'])
+#df = pd.read_csv('combined_zillow.csv')
 df['Year'] = df['Date'].dt.year
 
 
